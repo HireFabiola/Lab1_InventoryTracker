c5a41f21d19cd7c903a1b58b5b30a172ae374684
Fabiola <fabiola.aurelien@gmail.com>
2026-04-02 14:57:35 -0400

    Addressed all runtime error issues in product file

--- src/models/Product.ts
+++ src/models/Product.ts
@@ -1,20 +1,18 @@
-class Product {
+abstract class Product {
     sku: string;
     name: string;
     price: number;
 
 
     constructor(sku: string, name: string, price: number) {
-        this.sku = string;
-        this.name = string;
-        this.price = number;
+        this.sku = sku;
+        this.name = name;
+        this.price = price;
     }
 
-    displayDetails(): {
+    displayDetails(): string {
     return `Item ${this.sku} is ${this.name} and costs $${this.price}`;
     }
 
-    getPriceWithTax(taxRate):{
-         return this.price * (taxRate + 1);
+    abstract getPriceWithTax(): number;
     }
-}
