672d1ca143d2b0eab5b0146af8d7fb7df9e13ca6
Fabiola <fabiola.aurelien@gmail.com>
2026-04-02 18:09:19 -0400

    Completed DigitalProduct class with specified methods

--- src/models/DigitalProduct.ts
+++ src/models/DigitalProduct.ts
@@ -1,13 +1,23 @@
 import { Product } from './Product.js';
 
-class DigitalProduct extends Product{
+class DigitalProduct extends Product {
+
     fileSize: number;
 
-    getPriceWithTax():{
+    constructor(sku: string, name: string, price: number, fileSize: number) {
+        super(sku, name, price);
+        this.fileSize = fileSize;
+    }
+
+    getPriceWithTax(): number {
         return this.price;
     }
 
-    get fileSize(){
+    getFileSize(): string {
         return `${this.fileSize} megabytes`;
     }
 }
+let testProduct = new DigitalProduct("1225754", "test3", 10.00, 15);
+console.log(testProduct.getPriceWithTax());
+console.log(testProduct.getFileSize());
+console.log(testProduct.displayDetails());
